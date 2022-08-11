953843214db8ef087681b6e6fabad06ff8f9b33a
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-08-11 08:17:54 -0300

    Price Consumer

---
+++ stock-price/src/main/java/com/carlos/stockprice/services/RabbitMQService.java
@@ -0,0 +1,26 @@
+package com.carlos.stockprice.services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class RabbitMQService {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	public void sendMessage(String queueName, Object message) {
+		try {
+			String jsonMessage = this.objectMapper.writeValueAsString(message);
+			this.rabbitTemplate.convertAndSend(queueName, jsonMessage);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
