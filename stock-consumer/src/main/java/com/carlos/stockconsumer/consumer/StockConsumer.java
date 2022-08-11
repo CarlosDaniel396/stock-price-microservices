df3168696b2afc46449fb732c9fe5bf72c014094
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-08-11 09:01:12 -0300

    config: Prefetch Count

--- stock-consumer/src/main/java/com/carlos/stockconsumer/consumer/StockConsumer.java
+++ stock-consumer/src/main/java/com/carlos/stockconsumer/consumer/StockConsumer.java
@@ -3,6 +3,9 @@ package com.carlos.stockconsumer.consumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import constants.RabbitMQConstants;
 import dtos.StockDTO;
 
@@ -10,7 +13,9 @@ import dtos.StockDTO;
 public class StockConsumer {
 
 	@RabbitListener(queues = RabbitMQConstants.STOCK_QUEUE)
-	private void consumer(StockDTO stockDTO) {
+	private void consumer(String message) throws JsonProcessingException, InterruptedException {
+		StockDTO stockDTO = new ObjectMapper().readValue(message, StockDTO.class);
+
 		System.out.println(stockDTO.productCode);
 		System.out.println(stockDTO.quantity);
 		System.out.println("-------------------------");
